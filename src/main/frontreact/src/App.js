899c65b3343fb22b2ef174676b44b514d3a053c9
kangjun <kangjun@naver.com>
2024-11-15 15:06:57 +0900

    Test1

---
+++ src/main/frontreact/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div>
+      <h1>Spring Boot + React + NCP</h1>
+      <h3>카운트 : 1</h3>
+      <img src='./img/명수4.jpg' alt='명수님'/>
+      <h3>앙 명수띠</h3>
+    </div>
+  );
+};
+
+export default App;
